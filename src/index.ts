0f9dcd50093825038188c1fa1ba091494b29eba5
Matthew Abraham <wasteofintel@gmail.com>
2020-05-15 21:36:53 -0400

    misc

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import InstagramAuth from './authentication';
+import InstagramBasicService from './services';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const ig = InstagramAuth.getInstance({
+    AppID: process.env.IG_APP_ID || "",
+    ClientSecret: process.env.IG_CLIENT_SECRET || "",
+    RedirectURI: process.env.IG_REDIRECT_URI || "",
+    AccessToken: process.env.IG_ACCESS_TOKEN,
+    Scope: ['user_profile', 'user_media'],
+});
+
+const service = new InstagramBasicService(ig);
+const images = service.getMedia('complextt');
+console.log(images);
