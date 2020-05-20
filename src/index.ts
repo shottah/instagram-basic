cba202a2ceae940c4691d53cfed4de8f3b09fae6
Matthew Abraham <wasteofintel@gmail.com>
2020-05-20 17:18:20 -0400

    Change to package export structure.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import InstagramAuth from './authentication';
-import InstagramBasicService from './services';
-import dotenv from 'dotenv';
+import InstagramAuth from "./authentication";
+import InstagramBasicService from "./services";
 
-dotenv.config();
-
-const ig = InstagramAuth.getInstance({
-    AppID: process.env.IG_APP_ID || "",
-    ClientSecret: process.env.IG_CLIENT_SECRET || "",
-    RedirectURI: process.env.IG_REDIRECT_URI || "",
-    AccessToken: process.env.IG_ACCESS_TOKEN,
-    Scope: ['user_profile', 'user_media'],
-});
-
-const service = new InstagramBasicService(ig);
-const images = service.getMedia('complextt');
-images.then(res => console.log(res.data)).catch(err => console.log(err.response.status))
+export {InstagramBasicService}
+export {InstagramAuth}
