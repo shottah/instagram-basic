5074d42bd3de61b8577c5ea4c5056452af861da0
Matthew Abraham <wasteofintel@gmail.com>
2020-05-20 17:19:25 -0400

    Add some internal utility functions

--- src/authentication/index.ts
+++ src/authentication/index.ts
@@ -1,10 +1,10 @@
 import { API_URL } from '../config';
 
 export type AuthOptions = {
-    AppID: string,
-    ClientSecret: string,
-    RedirectURI: string,
-    AccessToken: string | undefined,
+    ClientID: String,
+    ClientSecret: String,
+    RedirectUri?: String,
+    AccessToken?: String,
     Scope: Array<String>,
 }
 
@@ -18,6 +18,12 @@ export default class InstagramAuth {
         InstagramAuth._options = options;
     }
 
+    /**
+     * This function returns the Singleton instance of 
+     * an InstagramAuth Object. This ensures that the 
+     * object is the same across all moments when 
+     * the software is executing.
+     */
     public static getInstance = (options: AuthOptions): InstagramAuth => {
         if (InstagramAuth._instance) return InstagramAuth._instance;
         else return InstagramAuth._instance = new InstagramAuth(options);
@@ -26,26 +32,40 @@ export default class InstagramAuth {
     /**
      * Constructs the URL Required to get the User's
      * Authorization Code.
-     * 
-     * @returns authorisation_url [String]
      */
     private constructAuthorisationUrl = (): String => {
         return (
             API_URL + 
             'oauth/authorize' +
-            '?client_id=' + InstagramAuth._options.AppID +
-            '&redirect_uri=' + InstagramAuth._options.RedirectURI +
+            '?client_id=' + InstagramAuth._options.ClientID +
+            '&redirect_uri=' + InstagramAuth._options.RedirectUri +
             '&scope=' + InstagramAuth._options.Scope.join(',') + 
-            '&response_type=code' 
+            '&response_type=code'
         );
     }
 
-    private getAuthorisationCode = (): {authorisation_code: String} => {
-        return {authorisation_code: ''};
+    /**
+     * Constructs the URL Required to get the User's 
+     * Long Lived Access Token.
+     */
+    private constructAccessTokenUrl = (authorisation_code: String): String => {
+        return (
+            API_URL +
+            'oauth/access_token' +
+            '?client_id=' + InstagramAuth._options.ClientID +
+            '&client_secret=' + InstagramAuth._options.ClientSecret +
+            '&grant_type=authorization_code' +
+            '&redirect_url=' + InstagramAuth._options.RedirectUri +
+            '&code=' + authorisation_code
+        );
     }
 
-    private getAccessToken = (authorisation_code: String): {access_token: String} => {
-        return {access_token: ''};
+    /**
+     * Sets the authorisation token received from the 
+     * Instagram Authorisation callback URL.
+     */
+    public authorise = (access_token: String): void => {
+        InstagramAuth._options.AccessToken = access_token;
     }
 
     /**
